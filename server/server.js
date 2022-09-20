205a6e4656d026204916930773693e0c12da678a
Sm4ck <mikeamartinez314@gmail.com>
2022-09-20 16:26:05 -0500

    Implement client-side GraphQL query for homepage data

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
+const path = require("path");
 const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
-const { authMiddleware } = require('./utils/auth');
+const { authMiddleware } = require("./utils/auth");
 
 const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
@@ -9,7 +10,7 @@ const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: authMiddleware
+  context: authMiddleware,
 });
 
 const app = express();
@@ -20,7 +21,14 @@ app.use(express.json());
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   server.applyMiddleware({ app });
+  // Serve up static assets
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../client/build")));
+  }
 
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  });
   db.once("open", () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
